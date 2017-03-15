76273ffb6b3ea4621da08e09ea8ded15ac0732b6
tlsprogramador <tlsprogramador@gmail.com>
2017-03-15 16:10:28 -0300

    Update script compre_ja.sql

--- script compre_ja.sql
+++ script compre_ja.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `zend-db`.`cliente` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `telefone` VARCHAR(11) NOT NULL,
+  `telefone` VARCHAR(15) NOT NULL,
   `regiao` SET('Norte', 'Nordeste', 'Sul', 'Sudeste', 'Centro-Oeste') NOT NULL,
   `unidade` SET('Porto Alegre', 'Curitiba', 'São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Brasília', 'Salvador', 'Recife', 'INDISPONÍVEL') NOT NULL,
   `data_nascimento` DATE NOT NULL,
